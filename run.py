3775067a2d284300183cb6128df6f6af86eea2a4
robin3910 <robin3910@163.com>
2022-10-20 00:13:48 +0800

    update: change send message interval

--- run.py
+++ run.py
@@ -13,8 +13,8 @@ with open('infos/usernames.txt', 'r') as f:
 # 带符号的话无法识别出链接，所以pdf和邮箱另起了一行
 message = ["Beiens(a 10-year brand aiming at learning toys) is looking for brand partners for New Product Release! Beiens®mental math game is expected to be released in your country in November. The sales revenue of it in China has exceeded 10 million US dollars.　　　　　　　　　　　　　　　　　　I reviewed your profile and believe it matches our criteria. As the brand partner you will: 　　　　　　　　　　　　　✔️First to get FREE beiens®mental math game ✔️Receive promo code and earn commission on every sale　　　　　　　　　✔️Get more traffic, fans, and commissions as the first launch influencers　　　　　　　　　✔️Be promoted on our social medias　　　　　　　　　✔️Get a chance to become our brand ambassador",
            "Features of beiens®mental math game:　　　　　　　　　　✔️Discover children's interest and improve ability of mental math　　　　　　　　　　　　✔️4 games, learning math through play　　　　　　　　　✔️3 levels, 17 types of questions　　　　　　　　　✔️10 million+ question bank for pre-K to grade 6 (4-12 years)　　　　　　✔️Automatic question generation and correction",
-           "Learn more: https://bit.ly/3AF0tA4",
-           "Please DM if interested! Or email to beiensmarketing@gmail.com"]
+           "Learn more: https://bit.ly/3C2ix8i",
+           "If interested Please DM to beiens_official or email to beiensmarketing@gmail.com"]
 
 
 while True:
@@ -37,3 +37,5 @@ while True:
             # Send message
             insta.sendMessage(
                 user=username, message=message)
+            print("sleep 5-15 min")
+            insta.__random_sleep__(300, 900)
